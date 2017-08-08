83dc104f20d504e082f6c089c574cc0527ede336
Adam Biser <adambiser@gmail.com>
2017-08-08 01:25:12 -0400

    Added Resources class and button images.

---
+++ extractor/ui/Resources.py
@@ -0,0 +1,10 @@
+import os
+import tkinter.tix as tix
+
+class Resources(object):
+    _path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "resources")
+
+    @classmethod
+    def getimage(cls, filename):
+        return tix.PhotoImage(file=os.path.join(Resources._path, filename))
+
