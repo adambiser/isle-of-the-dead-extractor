a9bc5785972ced4fcba2cb13356b8d4f69a087cb
Adam Biser <adambiser@gmail.com>
2017-08-11 01:02:01 -0400

    Added app icon.

--- extractor/resources.py
+++ extractor/resources.py
@@ -2,9 +2,9 @@ import os
 import tkinter.tix as tix
 
 
-class Resources(object):
-    _path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "resources")
+class Resources:
+    PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "resources")
 
     @classmethod
     def getimage(cls, filename):
-        return tix.PhotoImage(file=os.path.join(Resources._path, filename))
+        return tix.PhotoImage(file=os.path.join(Resources.PATH, filename))
